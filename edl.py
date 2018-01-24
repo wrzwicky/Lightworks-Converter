a46a622f2189ac1996aa1311d474dede4479fdc8
William R. Zwicky <wrzwicky@pobox.com>
2018-01-23 21:30:53 -0800

    Add notes.

--- edl.py
+++ edl.py
@@ -25,38 +25,54 @@ class EDLBlock:
         """Num, 3 digits, officially 001-999. Non-num makes row a comment."""
         self.reel = None
         """Reference to media file or tape.
-           Officially: 4-digit num, optional B; or BL for black,
+           Officially: 3-digit num 001-253, optionally followed by B;
+           or BL for black,
            or AX for aux source.
            Unofficially: any string."""
         self.channels = None
-        """A=audio1,V=video1,B=A+V,A2=audio2,A2/V=A2+V,AA=A1+A2,AA/V=A1+A2+V"""
+        """The channels involved in the edit.
+           A = audio1,
+           V = video1,
+           B = A+V,
+           A2 = audio2,
+           A2/V = A2+V,
+           AA = A1+A2,
+           AA/V = A1+A2+V"""
         self.transition = None
-        """C=cut,
+        """Type of edit statment.
+           C=cut,
            D=dissolve,
            Wxxx=wipe type xxx,
-           KB=key background,
-           K=key foreground,
-           KO=key foreground mask"""
+           KB = key background (background clip for matching K or KO line),
+           K=key foreground (foreground clip for matching KB line),
+           KO=key foreground mask ('this material being removed from the foreground' for matching KB line)"""
         self.transDur = None
-        """3-digit duration, in frames, or lone F"""
+        """Duration of transition.
+           3-digit int 001-255, in frames,
+           or lone F to 'indicate a fade condition'"""
         self.srcIn = None
-        """timecode (hh:mm:ss:ff)"""
+        """timecode (hh:mm:ss:ff)
+           'play source in-time' (start of clip in source file)"""
         self.srcOut = None
-        """timecode (hh:mm:ss:ff)"""
+        """timecode (hh:mm:ss:ff)
+           'play source out-time' (end of clip in source file)"""
         self.recIn = None
-        """timecode (hh:mm:ss:ff)"""
+        """timecode (hh:mm:ss:ff)
+           'record in-time' (where to place start of clip in output stream)"""
         self.recOut = None
         """timecode (hh:mm:ss:ff). Either out-time or duration.
            Ignored on read; clip length is srcOut-srcIn."""
 
 class EDL(list):
     def __init__(self):
-        self.title = None
-        self.dropframe = True
-        self.reels = {}
+        self.title = None      #str 'title' field in file
+        self.dropframe = True  #bool whether drop-frame (may want to hide this in timecode string instead)
+        #self.fps   = 23.97  #float frames per second
+        self.reels = {}        #str->str mapping from EDLBlock.reel to file path
         #self.edits = []
 
-    def load(filename):
+    def load(self, filename):
+        # auto-detect dialect: Premiere, Final Cut, Sony, CMX, etc.
         pass
 
     def savePremiere(self):
@@ -66,8 +82,8 @@ class EDL(list):
         #   003  E00706EU  V     D    030 00:00:26:29 00:00:32:10 00:00:01:02 00:00:07:13
         #   111^^22222222^^3333^^4444^555^66666666666^77777777777^88888888888^99999999999
         # Export from Premiere:
-        # 003  AX       AA    C        00:00:00:10 00:02:03:24 00:00:53:25 00:02:57:09
-        # * FROM CLIP NAME: Ep6_Sc2 - Elliot tries again with Tiff.mp4
+        #   003  AX       AA    C        00:00:00:10 00:02:03:24 00:00:53:25 00:02:57:09
+        #   * FROM CLIP NAME: Ep6_Sc2 - Elliot tries again with Tiff.mp4
 
         if not not self.title:
             print("TITLE: ", self.title)
@@ -83,7 +99,6 @@ class EDL(list):
                    block.transition, block.transDur,
                    block.srcIn, block.srcOut, block.recIn, block.recOut)
             print(s)
-
 
 
 # TITLE: title
